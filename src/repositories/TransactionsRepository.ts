2354ebdca4030baaf1e728f9ed6f35b9c682b62c
Alex Oliveira Carmo <alex.carmo91@gmail.com>
2020-05-26 13:09:06 -0300

    Finalizado o desafio de fundamentos

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,11 @@
+import { request } from 'express';
 import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +21,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, elemento) => {
+      if (elemento.type === 'income') {
+        // eslint-disable-next-line no-param-reassign
+        total += elemento.value;
+      }
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, elemento) => {
+      if (elemento.type === 'outcome') {
+        // eslint-disable-next-line no-param-reassign
+        total += elemento.value;
+      }
+      return total;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    this.getBalance();
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
